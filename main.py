d4875f2ed6ca81483db21ce732821edb5a73fdd9
ulaulalruaje <147150054+ulaulalruaje@users.noreply.github.com>
2023-10-09 23:03:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,75 @@ except: __file__ = os.getcwd()
 base_dir=path.dirname(path.abspath(__file__))
 ext_file=path.join(base_dir, 'ext.crx')
 ext_dir=path.join(base_dir, 'ext')
+prext_dir=path.join(base_dir, 'pr_ext')
 
 PROXY = 'http://73738zbpcibedc0-session-jrndidbridbw-lifetime-20:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
+
+up, hp = PROXY.replace('http://','').replace('https://','').split('@')
+username, password = up.split(':')
+host, port = hp.split(':')
+
+try:
+    os.mkdir(prext_dir)
+except FileExistsError:
+    pass
+
+manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Proxies",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {
+            "scripts": ["background.js"]
+        },
+        "minimum_chrome_version":"22.0.0"
+    }
+"""
+
+background_js = """
+    var config = {
+            mode: "fixed_servers",
+            rules: {
+            singleProxy: {
+                scheme: "http",
+                host: "%s",
+                port: parseInt(%s)
+            },
+            bypassList: ["localhost"]
+            }
+        };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "%s",
+                password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+                callbackFn,
+                {urls: ["<all_urls>"]},
+                ['blocking']
+    );
+""" % (host, port, username, password)
+
+with open(path.join(prext_dir, "manifest.json"), 'w') as m_file:
+    m_file.write(manifest_json)
+with open(path.join(prext_dir, "background.js"), 'w') as b_file:
+    b_file.write(background_js)
 
 options = uc.ChromeOptions()
 options.add_argument('--no-sandbox')
@@ -33,10 +100,7 @@ with zipfile.ZipFile(ext_file, 'r') as zip:
     zip.extractall(ext_dir)
 
 options.add_argument('--load-extension='+ext_dir)
-
-proxy=None
-#proxy='http://73738zbpcibedc0-session-dhhiffydghdch-lifetime-20:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
-
+options.add_argument('--load-extension='+prext_dir)
 
 driver = uc.Chrome(options=options)
 
