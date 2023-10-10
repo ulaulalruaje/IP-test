deac415a60b99b2bfbafc395d0409f00e4f4f704
ulaulalruaje <147150054+ulaulalruaje@users.noreply.github.com>
2023-10-10 17:21:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from time import sleep
 from os import path
 import requests, zipfile, os
 
-if os.name != 'nt':
-    # apt install xvfb
-    from pyvirtualdisplay import Display
-    display = Display(visible=0, size=(1360, 768))
-    display.start()
-
 try: __file__
 except: __file__ = os.getcwd()
 
@@ -17,73 +11,77 @@ ext_file=path.join(base_dir, 'ext.crx')
 ext_dir=path.join(base_dir, 'ext')
 prext_dir=path.join(base_dir, 'pr_ext')
 
-PROXY = 'http://73738zbpcibedc0-session-jrndidbridbw-lifetime-20:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
+PROXY = 'http://73738zbpcibedc0-session-jrndridneidbw-lifetime-20:okjz6fk9nkkx3rc@rp.proxyscrape.com:6060'
 
-up, hp = PROXY.replace('http://','').replace('https://','').split('@')
-username, password = up.split(':')
-host, port = hp.split(':')
-
-try:
-    os.mkdir(prext_dir)
-except FileExistsError:
-    pass
-
-manifest_json = """
-    {
-        "version": "1.0.0",
-        "manifest_version": 2,
-        "name": "Proxies",
-        "permissions": [
-            "proxy",
-            "tabs",
-            "unlimitedStorage",
-            "storage",
-            "<all_urls>",
-            "webRequest",
-            "webRequestBlocking"
-        ],
-        "background": {
-            "scripts": ["background.js"]
-        },
-        "minimum_chrome_version":"22.0.0"
-    }
-"""
-
-background_js = """
-    var config = {
-            mode: "fixed_servers",
-            rules: {
-            singleProxy: {
-                scheme: "http",
-                host: "%s",
-                port: parseInt(%s)
+if PROXY:
+    up, hp = PROXY.replace('http://','').replace('https://','').split('@')
+    username, password = up.split(':')
+    host, port = hp.split(':')
+    
+    try:
+        os.mkdir(prext_dir)
+    except FileExistsError:
+        pass
+    
+    manifest_json = """
+        {
+            "version": "1.0.0",
+            "manifest_version": 2,
+            "name": "Proxies",
+            "permissions": [
+                "proxy",
+                "tabs",
+                "unlimitedStorage",
+                "storage",
+                "<all_urls>",
+                "webRequest",
+                "webRequestBlocking"
+            ],
+            "background": {
+                "scripts": ["background.js"]
             },
-            bypassList: ["localhost"]
-            }
-        };
+            "minimum_chrome_version":"22.0.0"
+        }
+    """
+    
+    background_js = """
+        var config = {
+                mode: "fixed_servers",
+                rules: {
+                singleProxy: {
+                    scheme: "http",
+                    host: "%s",
+                    port: parseInt(%s)
+                },
+                bypassList: ["localhost"]
+                }
+            };
+    
+        chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+    
+        function callbackFn(details) {
+            return {
+                authCredentials: {
+                    username: "%s",
+                    password: "%s"
+                }
+            };
+        }
+    
+        chrome.webRequest.onAuthRequired.addListener(
+                    callbackFn,
+                    {urls: ["<all_urls>"]},
+                    ['blocking']
+        );
+    """ % (host, port, username, password)
+    
+    with open(path.join(prext_dir, "manifest.json"), 'w') as m_file:
+        m_file.write(manifest_json)
+    with open(path.join(prext_dir, "background.js"), 'w') as b_file:
+        b_file.write(background_js)
 
-    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
 
-    function callbackFn(details) {
-        return {
-            authCredentials: {
-                username: "%s",
-                password: "%s"
-            }
-        };
-    }
 
-    chrome.webRequest.onAuthRequired.addListener(
-                callbackFn,
-                {urls: ["<all_urls>"]},
-                ['blocking']
-    );
-""" % (host, port, username, password)
-
-with open(path.join(prext_dir, "manifest.json"), 'w') as m_file:
-    m_file.write(manifest_json)
-with open(path.join(prext_dir, "background.js"), 'w') as b_file:
-    b_file.write(background_js)
 
 options = uc.ChromeOptions()
 options.add_argument('--no-sandbox')
@@ -98,33 +96,31 @@ with open(ext_file, 'wb') as f:
 with zipfile.ZipFile(ext_file, 'r') as zip:
     zip.extractall(ext_dir)
 
-options.add_argument('--load-extension='+ext_dir)
-#options.add_argument('--load-extension='+prext_dir)
+if PROXY:
+    options.add_argument(f'--load-extension={ext_dir},{prext_dir}')
+else:
+    options.add_argument(f'--load-extension={ext_dir}')
 
 driver = uc.Chrome(options=options)
 
 driver.maximize_window()
 driver.execute_script('window.open("https://vzu.us")')
 
-solved=False
-attempt=0
+sleep(30)
 
-while not solved:
-    sleep(5)
-    driver.switch_to.window(driver.window_handles[1])
-    if 'Just a moment' in driver.page_source:
-        driver.switch_to.window(driver.window_handles[0])
-    else:
-        solved=True
-        driver.switch_to.window(driver.window_handles[0])
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-
-    attempt+=1
-    if attempt >= 6:
+driver.switch_to.window(driver.window_handles[1])
+if not 'LoanOrHost' in driver.page_source:
         raise Exception('30sec passed but captcha couldn\'t solve.')
 
-print(driver.page_source)
+user_agent = driver.execute_script("return navigator.userAgent;")
+cf_clearance = ''
+
+for c in driver.get_cookies():
+    if c['name'] == 'cf_clearance':
+        cf_clearance = c['value']
+
+print(user_agent)
+print(cf_clearance)
 driver.quit()
 
 
